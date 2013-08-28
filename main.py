4988337c46d8bfeb5777ec7494e8d02f0d484b21
Tony Narlock <tony@git-pull.com>
2013-08-28 16:06:20 +0800

    session, client, window, pane formats from http://sourceforge.net/p/tmux/tmux-code/ci/master/tree/format.c

--- main.py
+++ main.py
@@ -183,17 +183,110 @@ class Pane(object):
         return "%s(%s)" % (self.__class__, self.__dict__)
 
 
-pprint(tmux('list-windows'))
-pprint(windows)
-
-
-def list_sessions():
+def get_sessions():
     for session in cut(tmux('list-sessions'), '-f1', '-d:'):
         yield Session(session.strip('\n'))
 
-sessions = list(list_sessions())
 
-for session in sessions:
+"""
+    experiment, ALL_FORMATS parsing for session, window, pane object and
+    remove blank values
+
+    http://sourceforge.net/p/tmux/tmux-code/ci/master/tree/format.c
+"""
+
+SESSION_FORMATS = [
+    'session_group',
+    'session_name',
+    'session_windows',
+    'session_width',
+    'session_height',
+    'session_id',
+    'session_grouped',
+    'session_group',
+    'session_created',
+    'session_created_string',
+    'session_attached',
+]
+
+CLIENT_FORMATS = [
+    'client_cwd',
+    'client_height',
+    'client_width',
+    'client_tty',
+    'client_termname',
+    'client_created',
+    'client_created_string',
+    'client_activity',
+    'client_activity_string',
+    'client_prefix',
+    'client_utf8',
+    'client_readonly',
+    'client_session',
+    'client_last_session',
+]
+
+WINDOW_FORMATS = [
+    # format_window()
+    'window_id',
+    'window_name',
+    'window_width',
+    'window_height',
+    'window_layout',
+    'window_panes',
+    # format_winlink()
+    'window_index',
+    'window_flags',
+    'window_active',
+    'window_bell_flag',
+    'window_activity_flag',
+    'window_silence_flag',
+]
+
+PANE_FORMATS = [
+    'history_size',
+    'history_limit',
+    'history_bytes',
+    'pane_index',
+    'pane_width',
+    'pane_height',
+    'pane_title',
+    'pane_id',
+    'pane_active',
+    'pane_dead',
+    'pane_in_mode',
+    'pane_synchronized',
+    'pane_tty',
+    'pane_pid',
+    'pane_start_command',
+    'pane_start_path',
+    'pane_current_path',
+    'pane_current_command',
+
+    'cursor_x',
+    'cursor_y',
+    'scroll_region_upper',
+    'scroll_region_lower',
+    'saved_cursor_x',
+    'saved_cursor_y',
+
+    'alternate_on',
+    'alternate_saved_x',
+    'alternate_saved_y',
+
+    'cursor_flag',
+    'insert_flag',
+    'keypad_cursor_flag',
+    'keypad_flag',
+    'wrap_flag',
+
+    'mouse_standard_flag',
+    'mouse_button_flag',
+    'mouse_any_flag',
+    'mouse_utf8_flag',
+]
+
+for session in get_sessions():
     pprint(session)
     for window in session.windows:
         for pane in window.panes:
