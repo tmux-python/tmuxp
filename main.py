f8894f04bf77013cb3df319cd7d34d3f49f3066c
Tony Narlock <tony@git-pull.com>
2013-08-30 10:50:10 +0800

    move formats to formats.py, import

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kaptan
 from sh import tmux, cut, ErrorReturnCode_1
 from pprint import pprint
+from formats import SESSION_FORMATS, WINDOW_FORMATS, PANE_FORMATS
 
 
 class SessionExists(Exception):
@@ -353,97 +354,6 @@ def get_sessions():
 
     for session in sessions:
         yield session
-
-
-SESSION_FORMATS = [
-    'session_name',
-    'session_windows',
-    'session_width',
-    'session_height',
-    'session_id',
-    'session_created',
-    'session_created_string',
-    'session_attached',
-    'session_grouped',
-    'session_group',
-]
-
-CLIENT_FORMATS = [
-    'client_cwd',
-    'client_height',
-    'client_width',
-    'client_tty',
-    'client_termname',
-    'client_created',
-    'client_created_string',
-    'client_activity',
-    'client_activity_string',
-    'client_prefix',
-    'client_utf8',
-    'client_readonly',
-    'client_session',
-    'client_last_session',
-]
-
-WINDOW_FORMATS = [
-    # format_window()
-    'window_id',
-    'window_name',
-    'window_width',
-    'window_height',
-    'window_layout',
-    'window_panes',
-    # format_winlink()
-    'window_index',
-    'window_flags',
-    'window_active',
-    'window_bell_flag',
-    'window_activity_flag',
-    'window_silence_flag',
-]
-
-PANE_FORMATS = [
-    'history_size',
-    'history_limit',
-    'history_bytes',
-    'pane_index',
-    'pane_width',
-    'pane_height',
-    'pane_title',
-    'pane_id',
-    'pane_active',
-    'pane_dead',
-    'pane_in_mode',
-    'pane_synchronized',
-    'pane_tty',
-    'pane_pid',
-    'pane_start_command',
-    'pane_start_path',
-    'pane_current_path',
-    'pane_current_command',
-
-    'cursor_x',
-    'cursor_y',
-    'scroll_region_upper',
-    'scroll_region_lower',
-    'saved_cursor_x',
-    'saved_cursor_y',
-
-    'alternate_on',
-    'alternate_saved_x',
-    'alternate_saved_y',
-
-    'cursor_flag',
-    'insert_flag',
-    'keypad_cursor_flag',
-    'keypad_flag',
-    'wrap_flag',
-
-    'mouse_standard_flag',
-    'mouse_button_flag',
-    'mouse_any_flag',
-    'mouse_utf8_flag',
-]
 
 
 config = kaptan.Kaptan(handler="yaml")
