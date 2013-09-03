c1437d49abbeb2175f1bedbd1271aa59ca9a6145
Tony Narlock <tony@git-pull.com>
2013-09-03 12:38:39 +0800

    more work on tests

--- tests.py
+++ tests.py
@@ -10,21 +10,26 @@ root_logger.setLevel(logging.INFO)
 
 
 class TestClass(unittest.TestCase):
-    def setUp(self):
-        pass
+    '''
+        self.session
+            Session object
+        self.TEST_SESSION_NAME
+            string. name of the test case session.
+    '''
+    @classmethod
+    def setUpClass(cls):
+        try:
+            # atm bootstrap() retyrns name of session and the session object
+            cls.TEST_SESSION_NAME, cls.session = bootstrap()
+        except:
+            self.fail()
+        return
 
     def tearDown(self):
         pass
 
-    def test_1_session_creation(self):
-
-        t.switch_client('tony')
-
-        t.switch_client(TEST_SESSION_NAME)
-
-        assert t.has_session(TEST_SESSION_NAME) is True
-
-    def test_2_has_session_not_found(self):
+    def test_has_session(self):
+        assert t.has_session(self.TEST_SESSION_NAME) is True
         assert t.has_session('asdf2314324321') is False
 
     def test_3_switch_client_returns_session(self):
@@ -34,33 +39,35 @@ class TestClass(unittest.TestCase):
         pass
 
     def test_4_new_session_has_one_window(self):
-        self.assertEqual(1, len(session._windows))
+        self.assertEqual(1, len(self.session._windows))
 
     def test_5_session_split_window(self):
-        session.attached_window().split_window()
+        self.session.attached_window().split_window()
+        self.assertEqual(1, len(self.session._windows))
+
 
     def test_6_next(self):
-        session.attached_window().select_layout('even-horizontal')
-        session.attached_window().split_window()
+        self.session.attached_window().select_layout('even-horizontal')
+        self.session.attached_window().split_window()
         #session.sync_windows()
-        session.attached_window().split_window('-h')
+        self.session.attached_window().split_window('-h')
 
-        session.select_window(1)
+        self.session.select_window(1)
 
-        session.attached_window().select_pane(1)
-        session.attached_pane().send_keys('cd /srv/www/flaskr')
-        session.attached_window().select_pane(0)
-        session.attached_pane().send_keys('source .env/bin/activate')
-        session.new_window('second')
-        session.sync_windows()
-        self.assertEqual(2, len(session._windows))
-        session.new_window('testing 3')
-        session.sync_windows()
-        self.assertEqual(3, len(session._windows))
-        session.select_window(1)
-        session.kill_window(target_window='3')
-        session.sync_windows()
-        self.assertEqual(2, len(session._windows))
+        self.session.attached_window().select_pane(1)
+        self.session.attached_pane().send_keys('cd /srv/www/flaskr')
+        self.session.attached_window().select_pane(0)
+        self.session.attached_pane().send_keys('source .env/bin/activate')
+        self.session.new_window('second')
+        self.session.sync_windows()
+        self.assertEqual(2, len(self.session._windows))
+        self.session.new_window('testing 3')
+        self.session.sync_windows()
+        self.assertEqual(3, len(self.session._windows))
+        self.session.select_window(1)
+        self.session.kill_window(target_window='3')
+        self.session.sync_windows()
+        self.assertEqual(2, len(self.session._windows))
         #tmux('display-panes')
 
     def test_case_2(self):
@@ -70,9 +77,8 @@ class TestClass(unittest.TestCase):
         pass
 
 
-if __name__ == '__main__':
+def bootstrap():
     if t.has_clients():
-
         TEST_SESSION_PREFIX = 'tmxwrp_'
 
         # find current sessions prefixed with tmxwrp
@@ -104,6 +110,14 @@ if __name__ == '__main__':
             kill_session=True
         )
 
+        t.switch_client(TEST_SESSION_NAME)
+        print TEST_SESSION_NAME
+
+        return (TEST_SESSION_NAME, session)
+
+if __name__ == '__main__':
+
+    if t.has_clients():
         unittest.main()
     else:
         print('must have a tmux client running')
