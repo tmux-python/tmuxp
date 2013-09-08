4940018f11a0d2ecf85e287a07ca375f34bb1fc0
Tony Narlock <tony@git-pull.com>
2013-09-08 22:06:22 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+tmuxp
+-----
+
+a python ORM for tmux.
+
+
+"""
+from setuptools import setup
+
+setup(
+    name='tmuxp',
+    version='0.1-dev',
+    url='http://github.com/tony/tmuxp/',
+    license='BSD',
+    author='Tony Narlock',
+    author_email='tony@git-pull.com',
+    description='An object mapper and workspace manager for tmux',
+    packages=['tmux', 'tmux.testsuite'],
+    include_package_data=True,
+    install_requires=[
+        'logutils',
+        'kaptan',
+        'sh'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "License :: OSI Approved :: BSD License",
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        "Topic :: Utilities",
+        "Topic :: System :: Shells",
+    ],
+)
