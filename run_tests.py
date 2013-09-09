aaffc06eed5c14c9c9ee6340854455edd8c4fad4
Tony Narlock <tony@git-pull.com>
2013-09-09 23:54:27 +0800

    holy shit

--- run_tests.py
+++ run_tests.py
@@ -4,10 +4,21 @@ import sys
 import os
 from tmuxp import t
 import tmuxp.testsuite
+try:
+    from sh import tmux as tmux, ErrorReturnCode_1
+except ImportError:
+    logging.warning('tmux must be installed and in PATH\'s to use tmuxp')
+
 
 tmux_path = sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 if tmux_path not in sys.path:
     sys.path.insert(0, tmux_path)
+
+
+def interact(line, stdin, process):
+    print line
+
+tmux('-C', _out=interact)
 
 
 def main():
