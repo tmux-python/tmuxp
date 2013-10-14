0f7c7764f9074dc5dcc7ed18db9de17110d5830b
Tony Narlock <tony@git-pull.com>
2013-10-14 15:57:58 +0800

    cli stuff

--- tmuxp/cli.py
+++ tmuxp/cli.py
@@ -8,13 +8,15 @@
 """
 
 import os
+import argparse
 import logging
+import config
 from . import log
 
 logger = logging.getLogger(__name__)
 
 
-def setupLogger(logger=None):
+def setupLogger(logger=None, level='INFO'):
     '''setup logging for CLI use.
 
     :param logger: instance of logger
@@ -25,24 +27,78 @@ def setupLogger(logger=None):
     if not logger.handlers:
         channel = logging.StreamHandler()
         channel.setFormatter(log.LogFormatter())
-        logger.setLevel('INFO')
+        logger.setLevel(level)
         logger.addHandler(channel)
 
 
 def startup(config_dir):
-    ''' set up cli interface '''
+    ''' Initialize CLI.
+
+    :param config_dir: Config directory to search
+    :type config_dir: string
+    '''
 
     if not os.path.exists(config_dir):
         os.makedirs(config_dir)
 
 
 def main():
-    setupLogger()
 
-    logger.info('output')
-    logger.info(__name__)
+    config_dir = os.path.expanduser('~/.tmuxp/')
+    cwd_dir = os.getcwd() + '/'
 
-    # does config dir exist
-    config_dir = os.path.expanduser('~/.tmuxp')
+    parser = argparse.ArgumentParser(
+        description='''\
+        Launch tmux workspace. Help documentation: <http://tmuxp.rtfd.org>.
+        ''',
+    )
 
-    startup(config_dir)
+    parser.add_argument(
+        dest='configs',
+        nargs='*',
+        type=str,
+        default=None,
+        help='''\
+        List of config files to launch session from.
+
+        Checks current working directory (%s) then $HOME/.tmuxp directory (%s).
+
+            $ tmuxp .
+
+        will check launch a ~/.pullv.yaml / ~/.pullv.json from the cwd.
+        ''' % (cwd_dir + '/', config_dir)
+    )
+
+    parser.add_argument('-l', '--list', dest='list_configs', action='store_true',
+                        help='List config files available')
+    parser.add_argument('--log-level', dest='log_level', default='INFO',
+                        help='Log level')
+
+    args = parser.parse_args()
+
+    setupLogger(level=args.log_level.upper())
+
+    if args.list_configs:
+        startup(config_dir)
+        configs_in_user = config.in_dir(config_dir)
+        configs_in_cwd = config.in_cwd()
+
+        output = '\n'
+
+        if not configs_in_user:
+            output += '# In %s: \n\tNone found.\n' % config_dir
+        else:
+            output += '# In %s: \n\t%s\n' % (
+                config_dir, ', '.join(configs_in_user)
+            )
+
+        if not configs_in_cwd:
+            output += '# In %s (current directory): \n\tNone found.\n' % cwd_dir
+        else:
+            output += '# In %s (current directory):\n\t%s' % (
+                cwd_dir, ', '.join(configs_in_user)
+            )
+
+        logger.info(output)
+    else:
+        logger.info(parser.print_help())
