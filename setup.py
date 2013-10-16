c0960cd5ebff78c0a04ebfb1d02d485e5e028845
Tony Narlock <tony@git-pull.com>
2013-10-17 00:30:08 +0800

    add pkg/tmuxp.zsh to scripts, 0.0.4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=['tmuxp', 'tmuxp.testsuite'],
     include_package_data=True,
     install_requires=requirements('requirements.pip'),
-    scripts=['pkg/tmuxp.bash'],
+    scripts=['pkg/tmuxp.bash', 'pkg/tmuxp.zsh'],
     entry_points=dict(console_scripts=['tmuxp=tmuxp:main']),
     classifiers=[
         'Development Status :: 3 - Alpha',
