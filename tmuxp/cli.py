3f4ee3aa232bcf7b702c2b207dab5c9373e50369
Tony Narlock <tony@git-pull.com>
2013-10-22 13:40:20 +0800

    CLI updates, wip

--- tmuxp/cli.py
+++ tmuxp/cli.py
@@ -137,65 +137,9 @@ def build_workspace(config_file, args):
         return
 
 
-def cli_parser():
 
-    parser = argparse.ArgumentParser(
-        description='''\
-        Launch tmux workspace. Help documentation: <http://tmuxp.rtfd.org>.
-        ''',
-    )
-
-    parser.add_argument(
-        dest='configs',
-        nargs='*',
-        type=str,
-        default=None,
-        help='''\
-        List of config files to launch session from.
-
-        Checks current working directory (%s) then $HOME/.tmuxp directory (%s).
-
-            $ tmuxp .
-
-        will check launch a ~/.pullv.yaml / ~/.pullv.json from the cwd.
-        ''' % (cwd_dir + '/', config_dir)
-    )
-
-    parser.add_argument('-L', dest='socket_name', default=None,
-                        metavar='socket-name')
-
-    parser.add_argument('-S', dest='socket_path', default=None,
-                        metavar='socket-path')
-
-    parser.add_argument(
-        '-l', '--list', dest='list_configs', action='store_true',
-        help='List config files available')
-
-    parser.add_argument(
-        '-v', '--version', dest='version', action='store_true',
-        help='Prints the tmuxp version')
-
-    parser.add_argument('--log-level', dest='log_level', default='INFO',
-                        help='Log level')
-
-    return parser
-
-def main():
-
-    parser = cli_parser()
-
-    args = parser.parse_args()
-
-    setupLogger(level=args.log_level.upper())
-    try:
-        util.version()
-    except Exception as e:
-        logger.error(e)
-        sys.exit()
-
-    if args.version:
-        print('tmuxp %s' % __version__)
-    elif args.list_configs:
+def subcommand_load(args):
+    if args.list_configs:
         startup(config_dir)
         configs_in_user = config.in_dir(config_dir)
         configs_in_cwd = config.in_cwd()
@@ -237,22 +181,155 @@ def main():
         parser.print_help()
 
 
-def complete(cline, cpoint):
+def subcommand_attach_session(args):
+    print('attac session')
+    for session_name in args.session_name:
+        print(session_name)
+
+    def session_complete(command, commands, ctext):
+        if ctext.startswith(command + ' '):
+            commands[:] = []
+            ctext_attach = ctext.replace(command + ' ', '')
+
+            sessions = [s.get('session_name') for s in t._sessions]
+            commands.extend([c for c in sessions if ctext_attach in c])
+
+def subcommand_kill_session(args):
+    print('kill session')
+    print(args)
+    print(type(args.session_name))
+    print(args.session_name)
+
+    for session_name in args.session_name:
+        print(session_name)
+
+    def session_complete(command, commands, ctext):
+        if ctext.startswith(command + ' '):
+            commands[:] = []
+            ctext_attach = ctext.replace(command + ' ', '')
+
+            sessions = [s.get('session_name') for s in t._sessions]
+            commands.extend([c for c in sessions if ctext_attach in c])
+
+
+def cli_parser():
+
+    parser = argparse.ArgumentParser(
+        description='''\
+        Launch tmux workspace. Help documentation: <http://tmuxp.rtfd.org>.
+        ''',
+    )
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='subcommands',
+                                        description='valid subcommands',
+                                        help='additional help')
+
+    kill_session = subparsers.add_parser('kill-session')
+    kill_session.set_defaults(callback=subcommand_kill_session)
+
+    kill_session.add_argument(
+        dest='session_name',
+        nargs='*',
+        type=str,
+        default=None,
+    )
+
+
+    attach_session = subparsers.add_parser('attach-session')
+    attach_session.set_defaults(callback=subcommand_attach_session)
+
+    load = subparsers.add_parser('load')
+
+    load.add_argument(
+        '-l', '--list', dest='list_configs', action='store_true',
+        help='List config files available')
+
+    load.add_argument(
+        dest='configs',
+        nargs='*',
+        type=str,
+        default=None,
+        help='''\
+        List of config files to launch session from.
+
+        Checks current working directory (%s) then $HOME/.tmuxp directory (%s).
+
+            $ tmuxp .
+
+        will check launch a ~/.pullv.yaml / ~/.pullv.json from the cwd.
+        ''' % (cwd_dir + '/', config_dir)
+    )
+    load.set_defaults(callback=subcommand_load)
+
+
+    parser.add_argument('--log-level', dest='log_level', default='INFO',
+                        metavar='log-level',
+                        help='Log level e.g. INFO, DEBUG, ERROR')
+
+    parser.add_argument('-L', dest='socket_name', default=None,
+                        metavar='socket-name')
+
+    parser.add_argument('-S', dest='socket_path', default=None,
+                        metavar='socket-path')
+
+    parser.add_argument(
+        '-v', '--version', dest='version', action='store_true',
+        help='Prints the tmuxp version')
+
+
+    return parser
+
+def main():
 
     parser = cli_parser()
+
     args = parser.parse_args()
 
+    setupLogger(level=args.log_level.upper())
+    try:
+        util.version()
+    except Exception as e:
+        logger.error(e)
+        sys.exit()
+
+    if args.callback is subcommand_load:
+        subcommand_load(args)
+    if args.callback is subcommand_attach_session:
+        subcommand_attach_session(args)
+    if args.callback is subcommand_kill_session:
+        subcommand_kill_session(args)
+    else:
+        if args.version:
+            print('tmuxp %s' % __version__)
+        elif args.kill_session:
+            print(args.kill_session)
+
+        parser.print_help()
+
+
+
+def complete(cline, cpoint):
+
+    # parser = argparse.ArgumentParser()
+    # args = parser.parse_args()
+    # parser.add_argument('-L', dest='socket_name', default=None,
+                        # metavar='socket-name')
+
+    # parser.add_argument('-S', dest='socket_path', default=None,
+                        # metavar='socket-path')
+
+
     commands = []
-    commands += config.in_dir(config_dir)
-    commands += config.in_cwd()
-    commands.extend(['attach', 'kill-session'])
+
+    commands.extend(['attach-session', 'kill-session', 'load'])
 
     ctext = cline.replace('tmuxp ', '')
     commands = [c for c in commands if ctext in c]
 
     t = Server(
-        socket_name=args.socket_name,
-        socket_path=args.socket_path
+        # socket_name=args.socket_name or None,
+        # socket_path=args.socket_path or None
     )
 
     def session_complete(command, commands, ctext):
@@ -263,8 +340,17 @@ def complete(cline, cpoint):
             sessions = [s.get('session_name') for s in t._sessions]
             commands.extend([c for c in sessions if ctext_attach in c])
 
+    def config_complete(command, commands, ctext):
+        if ctext.startswith(command + ' '):
+            commands[:] = []
+            ctext_subcommand_args = ctext.replace(command + ' ', '')
+            commands += config.in_dir(config_dir)
+            commands += config.in_cwd()
+            commands = [c for c in commands if ctext_subcommand_args in c]
+
     session_complete('attach', commands, ctext)
-    session_complete('list-sessions', commands, ctext)
     session_complete('kill-session', commands, ctext)
+
+    config_complete('load', commands, ctext)
 
     print(' \n'.join(commands))
