746d4cda691ebfcf3d8eb1e0a4bd237631e97e2a
Tony Narlock <tony@git-pull.com>
2013-10-25 06:58:36 +0800

    teamocil: initial tests for teamocil config adapter

---
+++ tmuxp/testsuite/test_config_teamocil.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, with_statement
+
+import os
+import shutil
+import unittest
+import kaptan
+from .. import config, exc
+from ..util import tmux
+
+from .. import log
+import logging
+
+logger = logging.getLogger(__name__)
+TMUXP_DIR = os.path.join(os.path.dirname(__file__), '.tmuxp')
+
+
+class TeamocilTest(unittest.TestCase):
+
+    '''
+    assumes the configuration has been imported into a python dict correctly.
+    '''
+    teamocil_yaml = """\
+    windows:
+    - name: "sample-two-panes"
+      root: "~/Code/sample/www"
+      layout: even-horizontal
+      panes:
+        - cmd: ["pwd", "ls -la"]
+        - cmd: "rails server --port 3000"
+    """
+
+    teamocil_dict = {
+        'windows': [{
+            'name': 'sample-two-panes',
+            'root': '~/Code/sample/www',
+            'layout': 'even-horizontal',
+            'panes': [
+                { 'cmd': [
+                    'pwd',
+                    'ls -la'
+                    ]
+                },
+                { 'cmd': 'rails server --port 3000' }
+            ]
+        }]
+    }
+
+    def test_config_to_dict(self):
+        '''
+        expands shell commands from string to list
+        '''
+        configparser = kaptan.Kaptan(handler='yaml')
+        test_config = configparser.import_config(self.teamocil_yaml)
+        yaml_to_dict = test_config.get()
+        self.assertDictEqual(yaml_to_dict, self.teamocil_dict)
+
+    def test_config_to_yaml(self):
+        '''teamocil yaml to tmuxp yaml config
+
+        use check_consistency to assert against
+        '''
+        pass
+
+
+class Teamocil2Test(unittest.TestCase):
+
+    '''
+    assumes the configuration has been imported into a python dict correctly.
+    '''
+    teamocil_yaml = """\
+    windows:
+    - name: "sample-four-panes"
+      root: "~/Code/sample/www"
+      layout: tiled
+      panes:
+        - cmd: "pwd"
+        - cmd: "pwd"
+        - cmd: "pwd"
+        - cmd: "pwd"
+    """
+
+    teamocil_dict = {
+        'windows': [{
+            'name': 'sample-four-panes',
+            'root': '~/Code/sample/www',
+            'layout': 'tiled',
+            'panes': [
+                { 'cmd': 'pwd' },
+                { 'cmd': 'pwd' },
+                { 'cmd': 'pwd' },
+                { 'cmd': 'pwd' },
+            ]
+        }]
+    }
+
+    def test_config_to_dict(self):
+        '''
+        expands shell commands from string to list
+        '''
+        configparser = kaptan.Kaptan(handler='yaml')
+        test_config = configparser.import_config(self.teamocil_yaml)
+        yaml_to_dict = test_config.get()
+        self.assertDictEqual(yaml_to_dict, self.teamocil_dict)
+
+
+class Teamocil3Test(unittest.TestCase):
+
+    '''
+    assumes the configuration has been imported into a python dict correctly.
+    '''
+
+    teamocil_yaml = """\
+    windows:
+    - name: "my-first-window"
+      root: "~/Projects/foo-www"
+      layout: even-vertical
+      filters:
+        before: "rbenv local 2.0.0-p0"
+        after: "echo 'I am done initializing this pane.'"
+      panes:
+        - cmd: "git status"
+        - cmd: "bundle exec rails server --port 4000"
+          focus: true
+        - cmd:
+          - "sudo service memcached start"
+          - "sudo service mongodb start"
+    """
+
+    teamocil_dict = {
+        'windows': [{
+            'name': 'my-first-window',
+            'root': '~/Projects/foo-www',
+            'layout': 'even-vertical',
+            'filters': {
+              'before': 'rbenv local 2.0.0-p0',
+              'after': 'echo \'I am done initializing this pane.\''
+            },
+            'panes': [
+                { 'cmd': 'git status' },
+                { 'cmd': 'bundle exec rails server --port 4000', 'focus': True },
+                { 'cmd': [
+                    'sudo service memcached start',
+                    'sudo service mongodb start',
+                ]}
+            ]
+        }]
+    }
+
+    def test_config_to_dict(self):
+        '''
+        expands shell commands from string to list
+        '''
+
+        self.maxDiff = None
+        configparser = kaptan.Kaptan(handler='yaml')
+        test_config = configparser.import_config(self.teamocil_yaml)
+        yaml_to_dict = test_config.get()
+        self.assertDictEqual(yaml_to_dict, self.teamocil_dict)
+
+if __name__ == '__main__':
+    unittest.main()
