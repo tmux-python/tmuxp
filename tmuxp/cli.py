a761ea1fce9adf473e8ed00e6dec2852a372130a
Tony Narlock <tony@git-pull.com>
2013-10-24 14:23:30 +0800

    Also prompt of DISABLE_AUTO_TITLE set to 'false'. update CHANGES

--- tmuxp/cli.py
+++ tmuxp/cli.py
@@ -388,14 +388,10 @@ def oh_my_zsh_auto_title():
     if 'SHELL' in os.environ and 'zsh' in os.environ.get('SHELL'):
         if os.path.exists(os.path.expanduser('~/.oh-my-zsh')):
             # oh-my-zsh exists
-            if 'DISABLE_AUTO_TITLE' not in os.environ or os.environ.get('DISABLE_AUTO_TITLE') is "true":
+            if 'DISABLE_AUTO_TITLE' not in os.environ or os.environ.get('DISABLE_AUTO_TITLE') == "false":
                 print('Please set:\n\n'
                       '\texport DISABLE_AUTO_TITLE = \'true\'\n\n'
                       'in ~/.zshrc or where your zsh profile is stored.\n'
                       'Remember the "export" at the beginning!\n\n'
                       'Then create a new shell or type:\n\n'
                       '\t$ source ~/.zshrc')
-
-# check for oh-my-zsh
-# you are using oh-my-zsh without DISABLE_AUTO_TITLE, this can cause unexpected
-# behavior with tmux window renaming. Let me uncomment for you?
