8485cebee8a2b397704534bbd84251ead42da4e5
Tony Narlock <tony@git-pull.com>
2013-10-26 05:45:03 +0800

    teamocil tests update

--- tmuxp/testsuite/test_config_teamocil.py
+++ tmuxp/testsuite/test_config_teamocil.py
@@ -15,6 +15,10 @@ logger = logging.getLogger(__name__)
 TMUXP_DIR = os.path.join(os.path.dirname(__file__), '.tmuxp')
 
 # todo,  change  'root' to a cd or start_directory
+# todo: width in pane -> main-pain-width
+# todo: with_env_var
+# todo: clear
+# todo: cmd_separator
 
 
 def teamocil_to_tmuxp(sconf):
@@ -172,6 +176,30 @@ class Teamocil2Test(unittest.TestCase):
         }]
     }
 
+    tmuxp_dict = {
+        'session_name': None,
+        'windows': [
+            {
+                'window_name': 'sample-four-panes',
+                'layout': 'tiled',
+                'panes': [
+                    {
+                        'shell_command': 'pwd'
+                    },
+                    {
+                        'shell_command': 'pwd'
+                    },
+                    {
+                        'shell_command': 'pwd'
+                    },
+                    {
+                        'shell_command': 'pwd'
+                    },
+                ]
+            }
+        ]
+    }
+
     def test_config_to_dict(self):
         configparser = kaptan.Kaptan(handler='yaml')
         test_config = configparser.import_config(self.teamocil_yaml)
@@ -219,6 +247,38 @@ class Teamocil3Test(unittest.TestCase):
         }]
     }
 
+    tmuxp_dict = {
+        'session_name': None,
+        'windows': [
+            {
+                'window_name': 'my-first-window',
+                'layout': 'even-vertical',
+                'shell_command_before': [
+                    'rbenv local 2.0.0-p0',
+                ],
+                'shell_command_after': [
+                    'echo \'I am done initializing this pane.\''
+                ],
+                'panes': [
+                    {
+                        'shell_command': 'git status'
+                    },
+                    {
+                        'shell_command': 'bundle exec rails server --port 4000',
+                        'focus': True
+                    },
+                    {
+                        'shell_command': [
+                            'sudo service memcached start',
+                            'sudo service mongodb start'
+                        ]
+                    }
+                ]
+            }
+
+        ]
+    }
+
     def test_config_to_dict(self):
         configparser = kaptan.Kaptan(handler='yaml')
         test_config = configparser.import_config(self.teamocil_yaml)
@@ -244,6 +304,20 @@ class Teamocil4Test(unittest.TestCase):
                 {'cmd': 'pwd'}
             ]
         }]
+    }
+
+    tmuxp_dict = {
+        'session_name': None,
+        'windows': [
+            {
+                'window_name': 'erb-example',
+                'panes': [
+                    {
+                        'shell_command': 'pwd'
+                    }
+                ]
+            }
+        ]
     }
 
     def test_config_to_dict(self):
@@ -478,7 +552,7 @@ class TeamocilLayoutsTest(unittest.TestCase):
         }
     }
 
-    tmuxp_yaml = [
+    tmuxp_dict = [
         {
             'session_name': 'two-windows',
             'windows': [
@@ -522,7 +596,90 @@ class TeamocilLayoutsTest(unittest.TestCase):
             [
                 {
                     'window_name': 'foo',
+                    'shell_command_before': [
+                        'echo first before filter',
+                        'echo second before filter',
+                    ],
+                    'shell_command_after': [
+                        'echo first after filter',
+                        'echo second after filter',
+                    ],
+                    'panes': [
+                        {
+                            'shell_command': "echo 'foo'"
+                        },
+                        {
+                            'shell_command': "echo 'foo again'",
+                        }
+                    ]
                 }
+            ]
+        },
+        {
+            'window-name': 'two-windows-with-custm-command-options',
+            'windows': [
+                {
+                    'window_name': 'foo',
+                    'clear': True,
+                    'layout': 'tiled',
+                    'panes': [
+                        {
+                            'shell_command': "echo 'foo'",
+                        },
+                        {
+                            'shell_command': "echo 'foo again'",
+                        }
+                    ]
+                },
+                {
+                    'window_name': 'bar',
+                    'panes': [
+                        {
+                            'shell_command': [
+                                "echo 'bar'",
+                                "echo 'bar in an array'"
+                            ],
+                            'target': 'bottom-right'
+                        },
+                        {
+                            'shell_command': [
+                                "echo 'bar again'"
+                            ],
+                            'focus': True,
+                        }
+                    ]
+
+                }
+            ]
+
+        },
+        {
+            'session_name': 'three-window-within-a-session',
+            'windows': [
+                {
+                    'window_name': 'first window',
+                    'panes': [
+                        {
+                            'shell_command': "echo 'foo'"
+                        },
+                    ]
+                },
+                {
+                    'window_name': 'second window',
+                    'panes': [
+                        {
+                            'shell_command': "echo 'foo'"
+                        },
+                    ]
+                },
+                {
+                    'window_name': 'third window',
+                    'panes': [
+                        {
+                            'shell_command': "echo 'foo'"
+                        },
+                    ]
+                },
             ]
         }
 
@@ -535,7 +692,8 @@ class TeamocilLayoutsTest(unittest.TestCase):
         yaml_to_dict = test_config.get()
         self.assertDictEqual(yaml_to_dict, self.teamocil_dict)
 
-        #self.assertDictEqual(teamocil_to_tmuxp(self.teamocil_dict), self.tmuxp_dict)
+        # self.assertDictEqual(teamocil_to_tmuxp(self.teamocil_dict),
+        # self.tmuxp_dict)
 
         ''' this configuration contains multiple sessions in a single file.
             tmuxp can split them into files, proceed?
