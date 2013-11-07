385946ebe1e8d61fb626f638e2668d5c1878538f
Tony Narlock <tony@git-pull.com>
2013-11-07 08:16:47 +0800

    look up _vendor by absolute instead of relative

--- tmuxp/log.py
+++ tmuxp/log.py
@@ -20,8 +20,8 @@ import logging
 import logging.handlers
 import sys
 import time
-from ._vendor.colorama import init
-from ._vendor.colorama import Fore, Back, Style
+from tmuxp._vendor.colorama import init
+from tmuxp._vendor.colorama import Fore, Back, Style
 from .util import unicode, bytes, basestring
 
 try:
