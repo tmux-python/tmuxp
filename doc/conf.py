95026d6e0bb0cdd78e1b71c2cde19580b4bbb37c
Tony Narlock <tony@git-pull.com>
2013-11-13 10:28:35 +0800

    Update aafig to use png by default on html output. Add Pillow to doc/requirements.pip

--- doc/conf.py
+++ doc/conf.py
@@ -276,4 +276,11 @@ texinfo_documents = [
 intersphinx_mapping = {'http://docs.python.org/': None}
 
 # aafig format, try to get working with pdf
-aafig_format = dict(latex='pdf', html='svg')
+aafig_format = dict(latex='pdf', html='png')
+
+aafig_default_options = dict(
+    scale=.75,
+    aspect=0.5,
+    proportional=True,
+    #linewidth=.5,
+)
