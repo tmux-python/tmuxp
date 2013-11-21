c8ba6ed51ea0bd7ccae945bc5acec1ec42d5b0b9
Tony Narlock <tony@git-pull.com>
2013-11-21 08:38:13 -0600

    Fix comment in workspacebuilder.

--- tmuxp/workspacebuilder.py
+++ tmuxp/workspacebuilder.py
@@ -29,7 +29,7 @@ class WorkspaceBuilder(object):
     The normal phase of loading is:
 
         1.  :term:`kaptan` imports json/yaml/ini. ``.get()`` returns python
-        :class:`dict`::
+            :class:`dict`::
 
                 import kaptan
                 sconf = kaptan.Kaptan(handler='yaml')
