e19147e0e6133b80ec7d0090734f31c109dd79b8
Tony Narlock <tony@git-pull.com>
2014-03-31 17:58:13 -0500

    tests: cli.py remove an unused config fixture.

--- tmuxp/testsuite/cli.py
+++ tmuxp/testsuite/cli.py
@@ -151,41 +151,6 @@ class FindConfigsTest(TestCase):
             shutil.rmtree(TMUXP_DIR)
         logger.debug('wiped %s' % TMUXP_DIR)
 
-sampleconfigdict = {
-    'session_name': 'sampleconfig',
-    'start_directory': '~',
-    'windows': [
-        {
-            'window_name': 'editor',
-            'panes': [
-                {
-                    'start_directory': '~',
-                    'shell_command': ['vim'],
-                },
-                {
-                    'shell_command': ['cowsay "hey"']
-                },
-            ],
-            'layout': 'main-verticle'
-        },
-        {
-            'window_name': 'logging', 'panes': [
-                {
-                    'shell_command': ['tail -F /var/log/syslog'],
-                    'start_directory':'/var/log'
-                }
-            ]
-        }, {
-            'options': {'automatic_rename': True, },
-            'panes': [
-                {
-                    'shell_command': ['htop']
-                }
-            ]
-        }
-    ]
-}
-
 
 def suite():
     suite = unittest.TestSuite()
