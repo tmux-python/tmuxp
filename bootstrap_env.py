770aeb4f2ab9d56a93f611f13afa6601fff097d5
Tony Narlock <tony@git-pull.com>
2015-04-10 05:04:13 -0500

    parens around __future__ import in bootstrap_env.py

--- bootstrap_env.py
+++ bootstrap_env.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import, division, print_function, \
-    with_statement, unicode_literals
-
+from __future__ import (
+    absolute_import, division, print_function, with_statement, unicode_literals
+)
 
 import os
 import sys
