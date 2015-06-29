75d5d6b3765049ce6cf22cc19e26001c75c0f84d
Thomas Ballinger <thomasballinger@gmail.com>
2015-06-28 20:20:05 -0400

    fix attached_sessions docs to reflect return type

--- tmuxp/server.py
+++ tmuxp/server.py
@@ -286,11 +286,11 @@ class Server(TmuxRelationalObject):
         return self
 
     def attached_sessions(self):
-        """Return active :class:`Session` object.
+        """Return active :class:`Session` objects.
 
         This will not work where multiple tmux sessions are attached.
 
-        :rtype: :class:`Server`
+        :rtype: :py:obj:`list` of :class:`Session`
 
         """
 
