1f9d1e40e2f7994620b0fe36090fba6d3f17e3ac
Tony Narlock <tony@git-pull.com>
2016-01-30 17:02:03 -0600

    Bump to version 0.10.0

--- tmuxp/__about__.py
+++ tmuxp/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'tmuxp'
 __package_name__ = 'tmuxp'
-__version__ = '0.9.3'
+__version__ = '0.10.0'
 __description__ = 'Save and load tmux sessions thru JSON, YAML configs. Control tmux through Python objects.'
 __email__ = 'tony@git-pull.com'
 __author__ = 'Tony Narlock'
