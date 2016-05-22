1b9227ba4f4b1c9e025a9a828a1ea67e41645c94
Tony Narlock <tony@git-pull.com>
2016-05-22 06:36:27 -0500

    decorate start_directory builder test with retries

--- tests/test_workspacebuilder.py
+++ tests/test_workspacebuilder.py
@@ -322,6 +322,7 @@ def test_blank_pane_count(session):
     assert len(window4._panes) == 2
 
 
+@flaky(max_runs=5, min_passes=1)
 def test_start_directory(session, tmpdir):
     yaml_config = loadfixture("workspacebuilder/start_directory.yaml")
     test_dir = str(tmpdir.mkdir('foo bar'))
