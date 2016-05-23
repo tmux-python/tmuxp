f3e931192a8404521d2abe47ff3bc89d33dbde94
Tony Narlock <tony@git-pull.com>
2016-05-23 02:48:30 -0500

    Remove header title from test_util

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Tests for utility functions in tmux.
-
-tmuxp.tests.util
-~~~~~~~~~~~~~~~~
-
-"""
+"""Tests for utility functions in tmux."""
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals, with_statement)
