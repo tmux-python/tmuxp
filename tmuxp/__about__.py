52e7ce1c2a28f094f034ddb439ffc8c6c245da27
Tony Narlock <tony@git-pull.com>
2017-02-24 16:04:27 -0600

    tag v1.2.6

--- tmuxp/__about__.py
+++ tmuxp/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'tmuxp'
 __package_name__ = 'tmuxp'
-__version__ = '1.2.5'
+__version__ = '1.2.6'
 __description__ = 'tmux session manager'
 __email__ = 'tony@git-pull.com'
 __author__ = 'Tony Narlock'
