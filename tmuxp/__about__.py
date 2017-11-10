07ae8a446f180d1f6047ee747aee26977772f997
Tony Narlock <tony@git-pull.com>
2017-11-10 07:30:27 -0600

    update to v1.3.5

--- tmuxp/__about__.py
+++ tmuxp/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'tmuxp'
 __package_name__ = 'tmuxp'
-__version__ = '1.3.4'
+__version__ = '1.3.5'
 __description__ = 'tmux session manager'
 __email__ = 'tony@git-pull.com'
 __author__ = 'Tony Narlock'
