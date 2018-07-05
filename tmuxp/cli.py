ceec5f8309cf0e625815f233c5bdf8be99a093f9
Andrea D'Amore <anddam@brapi.net>
2018-07-05 11:44:26 +0200

    Use XDG base directory specification for config files

--- tmuxp/cli.py
+++ tmuxp/cli.py
@@ -35,17 +35,31 @@ def get_config_dir():
     """
     Return tmuxp configuration directory.
 
-    Checks for ``TMUXP_CONFIGDIR`` environmental variable.
+    ``TMUXP_CONFIGDIR`` environmental variable has precedence, then XDG default
+    directory is checked, either from XDG_CONFIG_HOME environmental variable or
+    its default, then the old default ~/.tmuxp is returned for compatibility.
 
     Returns
     -------
     str :
         absolute path to tmuxp config directory
     """
-    if 'TMUXP_CONFIGDIR' in os.environ:
-        return os.path.expanduser(os.environ['TMUXP_CONFIGDIR'])
 
-    return os.path.expanduser('~/.tmuxp/')
+    paths = []
+    if 'TMUXP_CONFIGDIR' in os.environ:
+        paths.append(os.environ['TMUXP_CONFIGDIR'])
+    if 'XDG_CONFIG_HOME' in os.environ:
+        paths.append(os.environ['XDG_CONFIG_HOME'])
+    else:
+        paths.append('~/.config/tmuxp/')
+    paths.append('~/.tmuxp')
+
+    for path in paths:
+        path = os.path.expanduser(path)
+        if os.path.isdir(path):
+            return path
+    # Return last path as default if none of the previous ones matched
+    return path
 
 
 def get_tmuxinator_dir():
