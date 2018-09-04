5a8fb2dc1319196d2693a15284ce875a5d1cae21
Tony Narlock <tony@git-pull.com>
2018-09-03 21:27:53 -0500

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -39,8 +39,8 @@ class PyTest(TestCommand):
 setup(
     name=about['__title__'],
     version=about['__version__'],
-    url='http://github.com/tmux-python/tmuxp/',
-    download_url='https://pypi.python.org/pypi/tmuxp',
+    url=about['__github__'],
+    download_url=about['__pypi__'],
     license=about['__license__'],
     author=about['__author__'],
     author_email=about['__email__'],
