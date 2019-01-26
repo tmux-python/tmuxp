8bd8fe461fd8a1a3bad3613d073742ee7aa3e826
Tony Narlock <tony@git-pull.com>
2019-01-26 12:13:31 -0600

    Add Python 3.7 to classifiers

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: PyPy',
         "Topic :: Utilities",
         "Topic :: System :: Shells",
