eb8d21292d42c46bf1c26b1bc92dd48fd9a28c7f
Tony Narlock <tony@git-pull.com>
2019-05-12 15:30:22 -0500

    Fix issue URLs

--- doc/conf.py
+++ doc/conf.py
@@ -37,7 +37,7 @@ releases_document_name = ["history"]
 releases_issue_uri = "https://github.com/tmux-python/tmuxp/issues/%s"
 releases_release_uri = "https://github.com/tmux-python/tmuxp/tree/v%s"
 
-issues_github_path = about['__github__']
+issues_github_path = about['__github__'].replace('https://github.com/', '')
 
 templates_path = ['_templates']
 
