03bf7e22c102bb87e8a58730fa7a852c4a8316cd
Joseph Flinn <joseph.s.flinn@gmail.com>
2020-09-09 20:49:14 -0700

    Adding in test plugins. Got these plugins to programmitcally install with python for testing purposes. Started rudimentary config testing. Started working on the cli.load_plugins test.

---
+++ tests/fixtures/pluginsystem/plugins/tmuxp_plugin_one/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='tmuxp-plugin-one',
+    version='0.0.1',
+    author='Joseph Flinn',
+    author_email='joseph.s.flinn@gmail.com',
+    packages=setuptools.find_packages(),
+    description='A tmuxp plugin to test the plugin system',
+)
