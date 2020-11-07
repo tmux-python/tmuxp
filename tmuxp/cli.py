fec95e4d97f221e0ab72bb19f1d3efac4ae71673
Joseph Flinn <joseph.s.flinn@gmail.com>
2020-11-07 10:41:14 -0800

    fixing python2  incompatible code

--- tmuxp/cli.py
+++ tmuxp/cli.py
@@ -1085,12 +1085,10 @@ def command_debug_info():
         """
         Format tmux command response for tmuxp stdout.
         """
-        return '\n'.join(
-            [
-                *prepend_tab(std_resp.stdout),
-                click.style('\n'.join(prepend_tab(std_resp.stderr)), fg='red'),
-            ]
-        )
+        return [
+            '\n'.join(prepend_tab(std_resp.stdout)),
+            click.style('\n'.join(prepend_tab(std_resp.stderr)), fg='red'),
+        ]
 
     output = [
         output_break(),
