bf29390fd62c5cf00c61ac6412e119831d4f5a40
Tony Narlock <tony@git-pull.com>
2022-01-08 07:17:46 -0600

    chore: Drop python 3.6 support (deprecated / EOL December 2021)

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
