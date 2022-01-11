aadb4399d95f066d3b5ab6d83dff58dcb5c9b2c8
Tony Narlock <tony@git-pull.com>
2022-01-10 22:00:57 -0600

    Tag v1.9.4 - the one packaged with poetry

--- tmuxp/__about__.py
+++ tmuxp/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'tmuxp'
 __package_name__ = 'tmuxp'
-__version__ = '1.9.3'
+__version__ = '1.9.4'
 __description__ = 'tmux session manager'
 __email__ = 'tony@git-pull.com'
 __author__ = 'Tony Narlock'
