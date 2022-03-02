15bd0594b84b3654577099795e2f46efff16f3be
Tony Narlock <tony@git-pull.com>
2022-03-01 22:29:38 -0600

    docs: Move to furo theme, add pytest-watcher

--- Makefile
+++ Makefile
@@ -19,6 +19,9 @@ black:
 test:
 	poetry run py.test $(test)
 
+start:
+	$(MAKE) test && poetry run ptw .
+
 watch_test:
 	if command -v entr > /dev/null; then ${PY_FILES} | entr -c $(MAKE) test; else $(MAKE) test entr_warn; fi
 
@@ -33,6 +36,12 @@ serve_docs:
 
 dev_docs:
 	$(MAKE) -j watch_docs serve_docs
+
+start_docs:
+	$(MAKE) -C docs start
+
+design_docs:
+	$(MAKE) -C docs design
 
 flake8:
 	poetry run flake8
--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,8 @@
+# flake8: NOQA E5
 import inspect
 import os
 import sys
 from os.path import dirname, relpath
-
-import alagitpull
 
 import tmuxp
 
@@ -28,8 +27,12 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.linkcode",
     "aafig",
-    "alagitpull",
     "sphinx_issues",
+    "myst_parser",
+    "sphinx_click.ext",  # sphinx-click
+    "sphinx_inline_tabs",
+    "sphinx_copybutton",
+    "sphinxext.opengraph",
     "myst_parser",
 ]
 
@@ -51,41 +54,46 @@ release = "%s" % (about["__version__"])
 
 exclude_patterns = ["_build"]
 
-pygments_style = "sphinx"
+pygments_style = "monokai"
+pygments_dark_style = "monokai"
 
-html_theme_path = [alagitpull.get_path()]
-html_theme = "alagitpull"
+html_css_files = ["css/custom.css"]
 html_extra_path = ["manifest.json"]
 html_static_path = ["_static"]
+html_theme = "furo"
+html_theme_path = []
+html_theme_options = {
+    "light_logo": "img/tmuxp.svg",
+    "dark_logo": "img/tmuxp.svg",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": about["__github__"],
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+}
 html_sidebars = {
     "**": [
-        "about.html",
-        "navigation.html",
-        "relations.html",
-        "more.html",
-        "book.html",
-        "searchbox.html",
+        "sidebar/scroll-start.html",
+        "sidebar/brand.html",
+        "sidebar/search.html",
+        "sidebar/navigation.html",
+        "sidebar/projects.html",
+        "sidebar/scroll-end.html",
     ]
 }
 
-html_theme_options = {
-    "logo": "img/tmuxp.svg",
-    "github_user": "tmux-python",
-    "github_repo": "tmuxp",
-    "github_type": "star",
-    "github_banner": True,
-    "projects": alagitpull.projects,
-    "project_name": about["__title__"],
-    "project_title": about["__title__"],
-    "project_description": about["__description__"],
-    "project_url": about["__docs__"],
-    "show_meta_manifest_tag": True,
-    "show_meta_og_tags": True,
-    "show_meta_app_icon_tags": True,
-}
-
-alagitpull_internal_hosts = ["tmuxp.git-pull.com", "0.0.0.0"]
-alagitpull_external_hosts_new_window = True
+# sphinxext.opengraph
+ogp_site_url = about["__docs__"]
+ogp_image = "_static/img/icons/icon-192x192.png"
+ogp_desscription_length = about["__description__"]
+ogp_site_name = about["__title__"]
 
 htmlhelp_basename = "%sdoc" % about["__title__"]
 
