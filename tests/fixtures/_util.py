7959e22c152abd586dccceb09e54be1034c4e447
Tony Narlock <tony@git-pull.com>
2022-03-12 11:28:40 -0600

    test: Add write_config()

--- tests/fixtures/_util.py
+++ tests/fixtures/_util.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 
 def curjoin(_file):  # return filepath relative to __file__ (this file)
@@ -7,3 +8,11 @@ def curjoin(_file):  # return filepath relative to __file__ (this file)
 
 def loadfixture(_file):  # return fixture data, relative to __file__
     return open(curjoin(_file)).read()
+
+
+def write_config(
+    config_path: pathlib.Path, filename: str, content: str
+) -> pathlib.Path:
+    config = config_path / filename
+    config.write_text(content, encoding="utf-8")
+    return config
