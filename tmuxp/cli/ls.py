f54a04fc0994b8bfb9a08d1623bf13d41cce9d06
Tony Narlock <tony@git-pull.com>
2022-03-20 11:35:32 -0500

    chore(tmuxp ls): Fix doc string

--- tmuxp/cli/ls.py
+++ tmuxp/cli/ls.py
@@ -6,7 +6,10 @@ from .constants import VALID_CONFIG_DIR_FILE_EXTENSIONS
 from .utils import get_config_dir
 
 
-@click.command(name="ls", short_help=f"List configured sessions in {get_config_dir()}.")
+@click.command(
+    name="ls",
+    short_help="List configured sessions in :meth:`tmuxp.cli.utils.get_config_dir`.",
+)
 def command_ls():
     tmuxp_dir = get_config_dir()
     if os.path.exists(tmuxp_dir) and os.path.isdir(tmuxp_dir):
