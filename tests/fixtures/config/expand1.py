871c9224f4596ccc384ffac8a38721663d341bfa
Tony Narlock <tony@git-pull.com>
2022-08-06 07:14:47 -0500

    tests: Defer test fixtures interpolation
    Interpolation needs to be deferred for libtmux to do its magic

--- tests/fixtures/config/expand1.py
+++ tests/fixtures/config/expand1.py
@@ -29,49 +29,51 @@ before_config = {
     ],
 }
 
-after_config = {
-    "session_name": "sampleconfig",
-    "start_directory": os.path.expanduser("~"),
-    "windows": [
-        {
-            "window_name": "editor",
-            "panes": [
-                {"shell_command": [{"cmd": "vim"}, {"cmd": "top"}]},
-                {"shell_command": [{"cmd": "vim"}]},
-                {"shell_command": [{"cmd": 'cowsay "hey"'}]},
-            ],
-            "layout": "main-verticle",
-        },
-        {
-            "window_name": "logging",
-            "panes": [{"shell_command": [{"cmd": "tail -F /var/log/syslog"}]}],
-        },
-        {
-            "start_directory": "/var/log",
-            "options": {"automatic-rename": True},
-            "panes": [
-                {"shell_command": [{"cmd": "htop"}]},
-                {"shell_command": [{"cmd": "vim"}]},
-            ],
-        },
-        {
-            "start_directory": os.path.normpath(
-                os.path.join(os.path.expanduser("~"), "./")
-            ),
-            "panes": [{"shell_command": [{"cmd": "pwd"}]}],
-        },
-        {
-            "start_directory": os.path.normpath(
-                os.path.join(os.path.expanduser("~"), "./asdf")
-            ),
-            "panes": [{"shell_command": [{"cmd": "pwd"}]}],
-        },
-        {
-            "start_directory": os.path.normpath(
-                os.path.join(os.path.expanduser("~"), "../")
-            ),
-            "panes": [{"shell_command": [{"cmd": "pwd"}]}],
-        },
-        {"panes": [{"shell_command": [{"cmd": "top"}]}]},
-    ],
-}
+
+def after_config():
+    return {
+        "session_name": "sampleconfig",
+        "start_directory": os.path.expanduser("~"),
+        "windows": [
+            {
+                "window_name": "editor",
+                "panes": [
+                    {"shell_command": [{"cmd": "vim"}, {"cmd": "top"}]},
+                    {"shell_command": [{"cmd": "vim"}]},
+                    {"shell_command": [{"cmd": 'cowsay "hey"'}]},
+                ],
+                "layout": "main-verticle",
+            },
+            {
+                "window_name": "logging",
+                "panes": [{"shell_command": [{"cmd": "tail -F /var/log/syslog"}]}],
+            },
+            {
+                "start_directory": "/var/log",
+                "options": {"automatic-rename": True},
+                "panes": [
+                    {"shell_command": [{"cmd": "htop"}]},
+                    {"shell_command": [{"cmd": "vim"}]},
+                ],
+            },
+            {
+                "start_directory": os.path.normpath(
+                    os.path.join(os.path.expanduser("~"), "./")
+                ),
+                "panes": [{"shell_command": [{"cmd": "pwd"}]}],
+            },
+            {
+                "start_directory": os.path.normpath(
+                    os.path.join(os.path.expanduser("~"), "./asdf")
+                ),
+                "panes": [{"shell_command": [{"cmd": "pwd"}]}],
+            },
+            {
+                "start_directory": os.path.normpath(
+                    os.path.join(os.path.expanduser("~"), "../")
+                ),
+                "panes": [{"shell_command": [{"cmd": "pwd"}]}],
+            },
+            {"panes": [{"shell_command": [{"cmd": "top"}]}]},
+        ],
+    }
