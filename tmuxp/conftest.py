c64bb431e79ff5f34130959fb09b1fff95d57298
Tony Narlock <tony@git-pull.com>
2022-09-10 12:11:53 -0500

    test: Move even more fixtures to use libtmux's

--- tmuxp/conftest.py
+++ tmuxp/conftest.py
@@ -7,11 +7,8 @@ import typing as t
 import pytest
 
 from _pytest.doctest import DoctestItem
-from _pytest.fixtures import SubRequest
 
-from libtmux import exc
-from libtmux.server import Server
-from libtmux.test import TEST_SESSION_PREFIX, get_test_session_name, namer
+from libtmux.test import namer
 from tests.fixtures import utils as test_utils
 
 if t.TYPE_CHECKING:
@@ -57,74 +54,9 @@ def socket_name(request) -> str:
     return "tmuxp_test%s" % next(namer)
 
 
-@pytest.fixture(scope="function")
-def server(
-    request: SubRequest, monkeypatch: pytest.MonkeyPatch, socket_name: str
-) -> Server:
-    tmux = Server(socket_name=socket_name)
-
-    def fin() -> None:
-        tmux.kill_server()
-
-    request.addfinalizer(fin)
-
-    return tmux
-
-
-@pytest.fixture(scope="function")
-def session(server):
-    session_name = "tmuxp"
-
-    if not server.has_session(session_name):
-        server.cmd(
-            "-f",
-            "/dev/null",  # use a blank config to reduce side effects
-            "new-session",
-            "-d",  # detached
-            "-s",
-            session_name,
-            "/bin/sh",  # use /bin/sh as a shell to reduce side effects
-            # normally, it'd be -c, but new-session is special
-        )
-
-    # find current sessions prefixed with tmuxp
-    old_test_sessions = [
-        s.get("session_name")
-        for s in server._sessions
-        if s.get("session_name").startswith(TEST_SESSION_PREFIX)
-    ]
-
-    TEST_SESSION_NAME = get_test_session_name(server=server)
-
-    try:
-        session = server.new_session(session_name=TEST_SESSION_NAME)
-    except exc.LibTmuxException as e:
-        raise e
-
-    """
-    Make sure that tmuxp can :ref:`test_builder_visually` and switches to
-    the newly created session for that testcase.
-    """
-    session_id = session.get("session_id")
-    assert session_id is not None
-    try:
-        server.switch_client(target_session=session_id)
-    except exc.LibTmuxException:
-        # server.attach_session(session.get('session_id'))
-        pass
-
-    for old_test_session in old_test_sessions:
-        logger.debug("Old test test session %s found. Killing it." % old_test_session)
-        server.kill_session(old_test_session)
-    assert TEST_SESSION_NAME == session.get("session_name")
-    assert TEST_SESSION_NAME != "tmuxp"
-
-    return session
-
-
 @pytest.fixture(autouse=True)
 def add_doctest_fixtures(
-    request: SubRequest,
+    request: pytest.FixtureRequest,
     doctest_namespace: t.Dict[str, t.Any],
 ) -> None:
     if isinstance(request._pyfuncitem, DoctestItem) and shutil.which("tmux"):
