b39b14e20fd32bd88921bf0975d269d92bec3ba8
Tony Narlock <tony@git-pull.com>
2022-09-11 11:23:33 -0500

    refactor: Updates for src/ layout

--- docs/conf.py
+++ docs/conf.py
@@ -10,13 +10,14 @@ import tmuxp
 # Get the project root dir, which is the parent dir of this
 cwd = Path(__file__).parent
 project_root = cwd.parent
+src_root = project_root / "src"
 
-sys.path.insert(0, str(project_root))
+sys.path.insert(0, str(src_root))
 sys.path.insert(0, str(cwd / "_ext"))
 
 # package data
 about: t.Dict[str, str] = {}
-with open(project_root / "tmuxp" / "__about__.py") as fp:
+with open(src_root / "tmuxp" / "__about__.py") as fp:
     exec(fp.read(), about)
 
 extensions = [
@@ -215,16 +216,18 @@ def linkcode_resolve(domain, info):  # NOQA: C901
     fn = relpath(fn, start=dirname(tmuxp.__file__))
 
     if "dev" in about["__version__"]:
-        return "{}/blob/master/{}/{}{}".format(
+        return "{}/blob/master/{}/{}/{}{}".format(
             about["__github__"],
+            "src",
             about["__package_name__"],
             fn,
             linespec,
         )
     else:
-        return "{}/blob/v{}/{}/{}{}".format(
+        return "{}/blob/v{}/{}/{}/{}{}".format(
             about["__github__"],
             about["__version__"],
+            "src",
             about["__package_name__"],
             fn,
             linespec,
