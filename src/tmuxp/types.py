e6c3d41f7594baa787bbeda91260ca9c954c12b6
Tony Narlock <tony@git-pull.com>
2022-10-29 07:26:18 -0500

    ci(mypy): Add types for StrPath
    No TypeAlias available until 3.10, unless we want to including
    typing_extensions.

---
+++ src/tmuxp/types.py
@@ -0,0 +1,14 @@
+"""Internal :term:`type annotations <annotation>`
+
+Notes
+-----
+
+:class:`StrPath` and :class:`StrOrBytesPath` is based on `typeshed's`_.
+
+.. _typeshed's: https://github.com/python/typeshed/blob/9687d5/stdlib/_typeshed/__init__.pyi#L98
+"""  # NOQA E501
+from os import PathLike
+from typing import Union
+
+StrPath = Union[str, "PathLike[str]"]
+""":class:`os.PathLike` or :class:`str`"""
