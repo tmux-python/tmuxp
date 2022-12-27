9ab3630d21c8a54d641f1c8ca7593a592cb85211
Tony Narlock <tony@git-pull.com>
2022-12-27 11:06:00 -0600

    Tag v1.20.2 (libtmux 0.17.2)

--- src/tmuxp/__about__.py
+++ src/tmuxp/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "tmuxp"
 __package_name__ = "tmuxp"
-__version__ = "1.20.1"
+__version__ = "1.20.2"
 __description__ = "tmux session manager"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
