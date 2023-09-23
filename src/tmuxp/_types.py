74a01fc728dacc4721271fbe7d08a913f0f92b35
Tony Narlock <tony@git-pull.com>
2023-09-23 07:03:18 -0500

    feat(_types): TYPE_CHECKING-guarded types, add PluginConfigSchema

---
+++ src/tmuxp/_types.py
@@ -0,0 +1,27 @@
+"""Internal, :const:`typing.TYPE_CHECKING` guarded :term:`type annotations <annotation>`
+
+These are _not_ to be imported at runtime as `typing_extensions` is not
+bundled with tmuxp. Usage example:
+
+>>> import typing as t
+
+>>> if t.TYPE_CHECKING:
+...     from tmuxp._types import PluginConfigSchema
+...
+"""
+import typing as t
+
+from typing_extensions import NotRequired, TypedDict
+
+
+class PluginConfigSchema(TypedDict):
+    plugin_name: NotRequired[str]
+    tmux_min_version: NotRequired[str]
+    tmux_max_version: NotRequired[str]
+    tmux_version_incompatible: NotRequired[t.List[str]]
+    libtmux_min_version: NotRequired[str]
+    libtmux_max_version: NotRequired[str]
+    libtmux_version_incompatible: NotRequired[t.List[str]]
+    tmuxp_min_version: NotRequired[str]
+    tmuxp_max_version: NotRequired[str]
+    tmuxp_version_incompatible: NotRequired[t.List[str]]
