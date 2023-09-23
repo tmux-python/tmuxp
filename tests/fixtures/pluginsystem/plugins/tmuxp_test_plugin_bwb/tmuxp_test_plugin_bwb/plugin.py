1d310ed257bcf211be54c76c91a5c7fee1ca4693
Tony Narlock <tony@git-pull.com>
2023-09-23 08:09:32 -0500

    chore(mypy): Typings for tests/fixtures/pluginsystem/plugins

--- tests/fixtures/pluginsystem/plugins/tmuxp_test_plugin_bwb/tmuxp_test_plugin_bwb/plugin.py
+++ tests/fixtures/pluginsystem/plugins/tmuxp_test_plugin_bwb/tmuxp_test_plugin_bwb/plugin.py
@@ -1,9 +1,14 @@
+import typing as t
+
 from tmuxp.plugin import TmuxpPlugin
+
+if t.TYPE_CHECKING:
+    from libtmux.session import Session
 
 
 class PluginBeforeWorkspaceBuilder(TmuxpPlugin):
-    def __init__(self):
-        self.message = "[+] This is the Tmuxp Test Plugin"
+    def __init__(self) -> None:
+        self.message: str = "[+] This is the Tmuxp Test Plugin"
 
-    def before_workspace_builder(self, session):
+    def before_workspace_builder(self, session: "Session") -> None:
         session.rename_session("plugin_test_bwb")
--- tests/fixtures/pluginsystem/plugins/tmuxp_test_plugin_fail/tmuxp_test_plugin_fail/plugin.py
+++ tests/fixtures/pluginsystem/plugins/tmuxp_test_plugin_fail/tmuxp_test_plugin_fail/plugin.py
@@ -1,9 +1,14 @@
+import typing as t
+
 from tmuxp.plugin import TmuxpPlugin
+
+if t.TYPE_CHECKING:
+    from tmuxp._types import PluginConfigSchema
 
 
 class PluginFailVersion(TmuxpPlugin):
-    def __init__(self):
-        config = {
+    def __init__(self) -> None:
+        config: "PluginConfigSchema" = {
             "plugin_name": "tmuxp-plugin-fail-version",
             "tmuxp_max_version": "0.0.0",
         }
