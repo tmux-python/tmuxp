c9655771b2850a15748210a0e5311434c1b3779f
Tony Narlock <tony@git-pull.com>
2023-09-23 09:26:41 -0500

    chore(mypy): Fix typings for workspace/freezer

--- src/tmuxp/workspace/freezer.py
+++ src/tmuxp/workspace/freezer.py
@@ -28,7 +28,7 @@ def inline(workspace_dict: t.Dict[str, t.Any]) -> t.Any:
         workspace_dict["shell_command"] = workspace_dict["shell_command"][0]
 
         if len(workspace_dict.keys()) == 1:
-            workspace_dict = workspace_dict["shell_command"]
+            return workspace_dict["shell_command"]
     if (
         "shell_command_before" in workspace_dict
         and isinstance(workspace_dict["shell_command_before"], list)
