c656c19ec130e205fc133e5cc45105c8d76372a5
Tony Narlock <tony@git-pull.com>
2023-10-07 06:55:07 -0500

    docs: remove ogp_description_length
    WARNING: The config value 'ogp_description_length' has type 'str', defaults to 'int'.

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,6 @@ autodoc_default_options = {
 # sphinxext.opengraph
 ogp_site_url = about["__docs__"]
 ogp_image = "_static/img/icons/icon-192x192.png"
-ogp_description_length = about["__description__"]
 ogp_site_name = about["__title__"]
 
 # sphinx-copybutton
