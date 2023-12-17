2d9f30d58c4323ed69bfc1302f8c09d503dcddd1
Tony Narlock <tony@git-pull.com>
2023-12-17 14:23:28 -0600

    tests(fixtures[expand1]): Note pydocstyle updates

--- tests/fixtures/workspace/expand2.py
+++ tests/fixtures/workspace/expand2.py
@@ -1,13 +1,16 @@
+"""YAML examples of expansion of tmuxp configurations from shorthand style."""
 import pathlib
 
 from .. import utils as test_utils
 
 
 def unexpanded_yaml() -> str:
+    """Return unexpanded, shorthand YAML tmuxp configuration."""
     return test_utils.read_workspace_file("workspace/expand2-unexpanded.yaml")
 
 
 def expanded_yaml() -> str:
+    """Return expanded, verbose YAML tmuxp configuration."""
     return test_utils.read_workspace_file("workspace/expand2-expanded.yaml").format(
         HOME=str(pathlib.Path().home())
     )
