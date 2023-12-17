cfafabd5b17fa8832e5e8e183f2a76886a79c2d3
Tony Narlock <tony@git-pull.com>
2023-12-17 15:38:55 -0600

    tests(plugins[tmuxp_test_plugin_bwb]): Note pydocstyle updates

--- tests/fixtures/pluginsystem/plugins/tmuxp_test_plugin_bwb/tmuxp_test_plugin_bwb/plugin.py
+++ tests/fixtures/pluginsystem/plugins/tmuxp_test_plugin_bwb/tmuxp_test_plugin_bwb/plugin.py
@@ -1,3 +1,4 @@
+"""Tmuxp example plugin for before_worksplace_builder."""
 import typing as t
 
 from tmuxp.plugin import TmuxpPlugin
@@ -7,8 +8,11 @@ if t.TYPE_CHECKING:
 
 
 class PluginBeforeWorkspaceBuilder(TmuxpPlugin):
+    """Tmuxp plugin that runs before worksplace builder starts."""
+
     def __init__(self) -> None:
         self.message: str = "[+] This is the Tmuxp Test Plugin"
 
     def before_workspace_builder(self, session: "Session") -> None:
+        """Run hook before workspace builder begins."""
         session.rename_session("plugin_test_bwb")
