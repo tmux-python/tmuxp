39422d2ded9a275ee8eeff25c590f2538756c987
Tony Narlock <tony@git-pull.com>
2024-02-05 13:49:52 -0600

    tests: Stub out test for set_layout_hook()

---
+++ tests/cli/test_hooks.py
@@ -0,0 +1,11 @@
+import typing as t
+
+from tmuxp.cli.load import set_layout_hook
+
+if t.TYPE_CHECKING:
+    from libtmux.session import Session
+
+
+def test_set_layout_hook(session: "Session") -> None:
+    """Test set_layout_hook."""
+    set_layout_hook(session, "main-vertical")
