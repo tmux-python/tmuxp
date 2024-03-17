dd7b59c94de2841fe1730fa5c6da90130f21691c
Tony Narlock <tony@git-pull.com>
2024-03-17 09:26:24 -0500

    Tag v1.41.0 (libtmux v0.33.0)

--- src/tmuxp/__about__.py
+++ src/tmuxp/__about__.py
@@ -2,7 +2,7 @@
 
 __title__ = "tmuxp"
 __package_name__ = "tmuxp"
-__version__ = "1.41.0b0"
+__version__ = "1.41.0"
 __description__ = "tmux session manager"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
