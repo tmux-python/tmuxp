763da16876a291c08e51c307d69ea65ce0e2e1d6
Tony Narlock <tony@git-pull.com>
2024-11-25 07:15:58 -0600

    docs(conf) Fix UP031 (printf-string-formatting)
    https://docs.astral.sh/ruff/rules/printf-string-formatting/
    
    docs/conf.py:194:16: UP031 Use format specifiers instead of percent format
        |
    192 |         lineno = None
    193 |
    194 |     linespec = "#L%d-L%d" % (lineno, lineno + len(source) - 1) if lineno else ""
        |                ^^^^^^^^^^ UP031
    195 |
    196 |     fn = relpath(fn, start=pathlib.Path(vcspull.__file__).parent)
        |
        = help: Replace with format specifiers

--- docs/conf.py
+++ docs/conf.py
@@ -191,7 +191,7 @@ def linkcode_resolve(domain: str, info: t.Dict[str, str]) -> t.Union[None, str]:
     except Exception:
         lineno = None
 
-    linespec = "#L%d-L%d" % (lineno, lineno + len(source) - 1) if lineno else ""
+    linespec = f"#L{lineno}-L{lineno + len(source) - 1}" if lineno else ""
 
     fn = relpath(fn, start=pathlib.Path(tmuxp.__file__).parent)
 
