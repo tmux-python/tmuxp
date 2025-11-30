e0935aa0a191119a0134164ffabe917334516a66
Tony Narlock <tony@git-pull.com>
2025-11-30 06:13:13 -0600

    Tag v1.58.0 (Drop tmux <3.2)

--- src/tmuxp/__about__.py
+++ src/tmuxp/__about__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __title__ = "tmuxp"
 __package_name__ = "tmuxp"
-__version__ = "1.57.0"
+__version__ = "1.58.0"
 __description__ = "tmux session manager"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
