70e2b237ae0e02a0d77eab1b07832490c0dd63cb
Tony Narlock <tony@git-pull.com>
2026-01-11 09:35:51 -0600

    Tag v1.63.1 (cli color fix)

--- src/tmuxp/__about__.py
+++ src/tmuxp/__about__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __title__ = "tmuxp"
 __package_name__ = "tmuxp"
-__version__ = "1.63.0"
+__version__ = "1.63.1"
 __description__ = "tmux session manager"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
