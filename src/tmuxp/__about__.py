c09372a30cf190f54334fc2c5d808a54d0f450dd
Tony Narlock <tony@git-pull.com>
2026-06-27 14:12:36 -0500

    Tag v1.71.0
    Minor release bumping the libtmux floor to 0.59.0 and adding tmux 3.7
    to the supported and tested version range (#1057).

--- src/tmuxp/__about__.py
+++ src/tmuxp/__about__.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 __title__ = "tmuxp"
 __package_name__ = "tmuxp"
-__version__ = "1.70.1"
+__version__ = "1.71.0"
 __description__ = "tmux session manager"
 __email__ = "tony@git-pull.com"
 __author__ = "Tony Narlock"
